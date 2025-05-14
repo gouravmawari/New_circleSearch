92ac34a03e27e20c94c7a4dfd992b2e1871df038
gouravmawari <smawari1000@gmail.com>
2025-05-15 03:16:32 +0530

    for Live

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get("/receive",async(req,res)=>{
 const dbURI =  "mongodb+srv://guddu:guddu@cluster1.ved7bni.mongodb.net/yes?retryWrites=true&w=majority";
 mongoose.connect(dbURI ,{useNewUrlParser : true , useUnifiedTopology: true})
 .then((result)=>{const PORT = process.env.PORT || 8888;
-    app.listen(PORT, ()=>{
+    app.listen(PORT,'0.0.0.0',()=>{
         console.log("server is created")
     })})
 .catch((err)=>console.log(err))                     
